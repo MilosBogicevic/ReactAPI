a6eb708058c8756be7ad3ae2d5d6cd6ad789554c
unknown <milbog.development@gmail.com>
2020-03-15 11:18:31 +0100

    Project done.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          isLoaded: true,
+          items: json
+        });
+      });
+  }
+
+  render() {
+    let { isLoaded, items } = this.state;
+
+    if (!isLoaded) {
+      return <div>LOading....</div>;
+    } else {
+      return (
+        <div className="App">
+          <ul>
+            {items.map(item => (
+              <li key={item.id}>
+                <strong>Name:</strong> {item.name} <br />
+                <strong>Email:</strong> {item.email} <br />{" "}
+                <strong>City: </strong>
+                {item.address.city} <br /> <strong>Phone:</strong> {item.phone}
+                <br />
+                <br />
+              </li>
+            ))}
+          </ul>
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
